39ed1bcf14f8c108637ed8115e248eb7c98f8868
Bjarmi Árdal Bergsteinsson <bjarmi11@ru.is>
2014-06-09 16:21:43 +0000

    Doing stuff... JS makes no sense....

--- popup.js
+++ popup.js
@@ -6,30 +6,38 @@ document.addEventListener('DOMContentLoaded', function(){
 	document.getElementById('buyButt').addEventListener('click', goToBuy);
 });
 
+function searchForRoute(){
 
-function findTitleTag(html){
-  var reg = "<title>*</title>";
-  
-
+  var input = getInput();
+	chrome.tabs.create({ url: "http://www.straeto.is/" ,active:false});
+  var tab;
+  chrome.tabs.query( {url:"http://www.straeto.is/"},function(results){
+    tab = results[0];
+  });
+  //chrome.tabs.executeScript(ID, { file: "straeto.js" },function(){
+    chrome.tabs.sendMessage(tab.id, {action:"input",data: input},function(sender){
+      tab.active = true;
+    });
+  //});
+  //alert(input.origin);
 }
 
-function searchForRoute(){
-	var input = {};
-	input.origin = document.getElementById('inpOrig').value;
-	input.dest = document.getElementById('inpDest').value;
-	input.timeH = document.getElementById('timeH').options[document.getElementById('timeH').selectedIndex].value;
-	input.timeM = document.getElementById('timeM').options[document.getElementById('timeM').selectedIndex].value;
-	
-	if(document.getElementById('leave').checked){
-		input.timeRev = document.getElementById('leave').value;
-	} else {
-		input.timeRev = document.getElementById('arrive').value
-	}
-	
-	input.dateD = document.getElementById('dateD').options[document.getElementById('dateD').selectedIndex].value;
-	input.dateMY = document.getElementById('dateM').options[document.getElementById('dateM').selectedIndex].value;
-	console.log(input);
-	chrome.tabs.create({ url: "http://www.straeto.is/" });
+function getInput(){
+    var input = {};
+  input.origin = document.getElementById('inpOrig').value;
+  input.dest = document.getElementById('inpDest').value;
+  input.timeH = document.getElementById('timeH').options[document.getElementById('timeH').selectedIndex].value;
+  input.timeM = document.getElementById('timeM').options[document.getElementById('timeM').selectedIndex].value;
+  
+  if(document.getElementById('leave').checked){
+    input.timeRev = document.getElementById('leave').value;
+  } else {
+    input.timeRev = document.getElementById('arrive').value;
+  }
+  
+  input.dateD = document.getElementById('dateD').options[document.getElementById('dateD').selectedIndex].value;
+  input.dateMY = document.getElementById('dateM').options[document.getElementById('dateM').selectedIndex].value;
+  return input;
 }
 
 function goToMap(){
@@ -47,24 +55,24 @@ function goToBuy(){
 
 
 
-/*chrome.extension.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-    message.innerText = request.source;
-  }
-});*/
+//chrome.extension.onMessage.addListener(function(request, sender,callBack) {
+//  if (request.action == "InsertInput") {
+//    callBack
+//  }
+//});
 
 function onWindowLoad() {
 
-  var message = document.querySelector('#message');
+  //var message = document.querySelector('#message');
 
-  chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.extension.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
-    }
-  });
+  //chrome.tabs.executeScript(null, {
+  //  file: "straeto.js"
+  //}, function() {
+  //  // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+  //  if (chrome.extension.lastError) {
+  //    message.innerText = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
+  //  }
+  //});
 
 }
 
--- straeto.js
+++ straeto.js
@@ -1,1 +1,14 @@
-alert("ho0");
+function InjectInfo(data){
+	alert("HEY! LISTEN!");
+	var origin = document.querySelector('#originName');
+	var dest = 	document.querySelector('#destinationName');
+	var form = 	document.querySelector('#minimalJourneyRequestDetails');
+	origin.value = data.origin;
+	dest.value = data.dest;
+	form.submit();
+}
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    alert("HEY!");
+  //if (request.action == "input") {
+    InjectInfo(request.data);
+});
